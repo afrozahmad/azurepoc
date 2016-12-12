a64ac5c770c4959fc71d304ecee878537abfd795
aahmad <afroz_ahmad@gap.com>
2016-12-11 20:30:22 -0800

    more changes

--- hbase-spark/template/spark202-installer-v04.sh
+++ hbase-spark/template/spark202-installer-v04.sh
@@ -39,8 +39,8 @@ rm -f /tmp/spark-2.0.2-bin-hadoop2.6.tgz
 # Update/link files/variables necessary to make Spark work on HDInsight.
 
 echo "SPARK_DIST_CLASSPATH=$(hadoop classpath)$(hbase classpath)" | sudo tee -a /etc/environment
-echo "export SPARK_DIST_CLASSPATH=$(hadoop classpath)$(hbase classpath)"
-echo "export SPARK_MAJOR_VERSION=2"
+export SPARK_DIST_CLASSPATH=$(hadoop classpath)$(hbase classpath)
+export SPARK_MAJOR_VERSION=2
 
 
 ln -s /etc/hive/conf/hive-site.xml /usr/hdp/current/spark/conf
