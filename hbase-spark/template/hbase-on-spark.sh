f5caa5449091b10d7096713baeb324f1350058bf
aahmad <afroz_ahmad@gap.com>
2016-12-24 15:49:25 -0800

    scipt

--- hbase-spark/template/hbase-on-spark.sh
+++ hbase-spark/template/hbase-on-spark.sh
@@ -1,1 +1,12 @@
-scp sshuser@hbase-base-ssh.azurehdinsight.net:/etc/hbase/conf/hbase-site.xml /etc/hbase/conf/
+sudo apt-get install jq
+
+
+zk = $(curl -s -u admin:Password@123 -G https://hbase-base.azurehdinsight.net/api/v1/clusters/hbase-base/services/ZOOKEEPER/components/ZOOKEEPER_SERVER | jq '[.host_components[].HostRoles.host_name]|join(", ")')
+
+#wget hbase-config from git to the servers
+wget -O /tmp/hbase-config.xml -q https://hdiconfigactions.blob.core.windows.net/linuxconfigactionmodulev01/HDInsightUtilities-v01.sh
+
+
+
+sed "s/zookeeper/$zk/g" < /tmp/hbase-site.xml > /etc/hbase/conf/hbase-site.xml
+
