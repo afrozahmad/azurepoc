c7c097664de2b27c457718ea8d6ab939f5d3420a
aahmad <afroz_ahmad@gap.com>
2016-12-24 21:28:35 -0800

    more changes

--- hbase-spark/template/hbase-on-spark.sh
+++ hbase-spark/template/hbase-on-spark.sh
@@ -4,10 +4,10 @@ mv jq /tmp/jq
 
 
 
-zk = $(curl -s -u admin:Password@123 -G https://hbase-base.azurehdinsight.net/api/v1/clusters/hbase-base/services/ZOOKEEPER/components/ZOOKEEPER_SERVER | /tmp/jq '[.host_components[].HostRoles.host_name]|join(", ")')
+zk=$(curl -s -u admin:Password@123 -G https://hbase-base.azurehdinsight.net/api/v1/clusters/hbase-base/services/ZOOKEEPER/components/ZOOKEEPER_SERVER | /tmp/jq '[.host_components[].HostRoles.host_name]|join(", ")')
 
 #wget hbase-config from git to the servers
-wget -O /tmp/hbase-config.xml -q https://raw.githubusercontent.com/afrozahmad/azurepoc/master/hbase-spark/template/hbase-site.xml
+wget -O /tmp/hbase-site.xml -q https://raw.githubusercontent.com/afrozahmad/azurepoc/master/hbase-spark/template/hbase-site.xml
 
 
 rm -f /etc/hbase/conf/hbase-site.xml
